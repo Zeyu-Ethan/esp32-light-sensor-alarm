83625951befbfa5827a9ec1a4f781d2b64239741
Zeyu-Ethan <308696545+Zeyu-Ethan@users.noreply.github.com>
2026-08-03 18:10:42 +0100

    Wi-Fi Web Server Test

--- code/wifi_alarm_monitoring/wifi_alarm_monitoring.ino
+++ code/wifi_alarm_monitoring/wifi_alarm_monitoring.ino
@@ -1,13 +1,24 @@
+// ESP32 Light Sensor Alarm System
+// V3 Wi-Fi Web Server Test
+
 #include <WiFi.h>
+#include <WebServer.h>
 
 const char* ssid = "YOUR_WIFI_NAME";
 const char* password = "YOUR_WIFI_PASSWORD";
+
+WebServer server(80);
+
+void handleHomePage()
+{
+  server.send(200, "text/html", "<h1>ESP32 Web Server Test</h1><p>Wi-Fi web server is working.</p>");
+}
 
 void setup()
 {
   Serial.begin(115200);
 
-  Serial.println("ESP32 Wi-Fi connection test started");
+  Serial.println("ESP32 Wi-Fi web server test started");
   Serial.print("Connecting to Wi-Fi: ");
   Serial.println(ssid);
 
@@ -24,9 +35,14 @@ void setup()
 
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  server.on("/", handleHomePage);
+  server.begin();
+
+  Serial.println("Web server started");
 }
 
 void loop()
 {
-  
+  server.handleClient();
 }
